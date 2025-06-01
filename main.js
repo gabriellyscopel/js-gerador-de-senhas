6f8202758003a0c0c6181fa356c1610321effaef
gabriellyscopel <145457846+gabriellyscopel@users.noreply.github.com>
2025-06-01 16:58:03 -0300

    Update main.js

--- main.js
+++ main.js
@@ -72,11 +72,11 @@ function classificaSenha(tamanhoAlfabeto) {
     } else if (entropia <= 35) {
         forcaSenha.classList.add('fraca');
     }
-    const valorEntropia = document.querySelector('.entropia');
-    valorEntropia.textContent = "Um computador pode levar até " + Math.floor(2 ** entropia / (100e6 * 60 * 60 * 24)) + " dias para descobrir essa senha.";
-}
+    valorEntropia.textContent = Math.floor(2**entropia/(100e6*60*60*24));
 
+valorEntropia.textContent = "Um computador pode levar até " + Math.floor(2**entropia/(100e6*60*60*24));
 
+valorEntropia.textContent = "Um computador pode levar até " + Math.floor(2**entropia/(100e6*60*60*24)) + " dias para descobrir essa senha.";
 
 
 
